6a633737d440f3e7ec712ee0c7535664e34052b3
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-13 10:54:56 +0530

    added few more topics

--- src/main/java/Section_7/Video_51.java
+++ src/main/java/Section_7/Video_51.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-// No Need to watch video 51,53,55,56,58,59,60 again
-// Need to watch video 52,54,57 again
+// No Need to watch video 51,53,55,56,58,59,60,61,64,66,68 again
+// Need to watch video 52,54,57,62 again
 public class Video_51 {
     @Test
     void test(){
