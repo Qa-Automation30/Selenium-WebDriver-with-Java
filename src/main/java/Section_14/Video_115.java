004c9f64249288f5fbcf500504450cea778442d4
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-15 13:39:37 +0530

    added few more topics

--- src/main/java/Section_14/Video_115.java
+++ src/main/java/Section_14/Video_115.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 public class Video_115 {
     // No need to watch video 114
+    // Video 115 is very IMp to watch again and again
 
-    @Test
-    void test(){
+    @Test(enabled = false)
+    // This test is without using of Stream
+    void withOutUsingStreams(){
         // Learning Java Streams, Lambada Expressions for building optimized code->
         /**
          * 1--> "Streams" are the new "concept" and introduced in JAVA 8.
@@ -27,13 +29,13 @@ public class Video_115 {
         for(int i=0;i<nameList.size();i++){
            System.out.println(nameList.get(i));
         }
-        // Now find the person whose name starts with "Noop"
+        // Now find the person whose name is "Noop"
         for(int i=0;i<nameList.size();i++){
             if(nameList.get(i).equals("Noop")){
                 System.out.println(nameList.get(i));
             }
         }
-        // Now find the total number of person whose name Starts with "a" or "A"
+        // 3rd-> Now find the total number of person whose name Starts with "a" or "A"
         int count=0;
         for(int i=0;i<nameList.size();i++){
             if(nameList.get(i).toUpperCase().startsWith("A")){
@@ -41,6 +43,23 @@ public class Video_115 {
             }
         }
         System.out.println("Total no of persons whose name starts with A-->"+count);
+    }
+
+    @Test
+    void usingStreams(){
+        // Using Streams here to optimize our code
+        List<String> nameList = new ArrayList<>();
+        nameList.add("Abc");
+        nameList.add("xyz");
+        nameList.add("klm");
+        nameList.add("aqq");
+        nameList.add("Noop");
+        nameList.add("aff");
+        // This is the concept of Streams and you can reduce your code like below .[Compare with above testCases 3rd step]
+       long count = nameList.stream().filter(s->s.toUpperCase().startsWith("A")).count();
+       System.out.println(count);
+       // // Now find the person whose name is "Noop" using Streams
+        nameList.stream().filter(name->name.equals("test")).forEach(name->System.out.println(name));
 
     }
 }
