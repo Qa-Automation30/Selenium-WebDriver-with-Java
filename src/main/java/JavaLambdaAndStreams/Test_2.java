3e354fb8b21a0210ca7d4795711f783120e9aebe
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-20 10:31:39 +0530

    added few more topics

---
+++ src/main/java/JavaLambdaAndStreams/Test_2.java
@@ -0,0 +1,23 @@
+package JavaLambdaAndStreams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test_2 {
+    public static void main(String args[]) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        /**
+         * Now print only the even numbers from the list
+         */
+        list.stream().filter(num->num%2==0).forEach(num->System.out.println("Even number are-->"+num));
+    }
+}
