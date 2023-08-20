763cb283c4c473ad7b8f3c70c62d3ca381e36049
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-20 16:24:53 +0530

    added few more topics

--- src/main/java/JavaLambdaAndStreams/Test_13.java
+++ src/main/java/JavaLambdaAndStreams/Test_13.java
@@ -9,11 +9,31 @@ public class Test_13 {
     public static void main(String args[]){
         List<Course> list = new ArrayList<>();
         list.add( new Course("Azure","Devops","3","101"));
-        list.add( new Course("Aws","Devops","2.5","114"));
-        list.add( new Course("Selenium","Automation","3.9","125"));
-        list.add( new Course("Cypress","Automation","3.5","22"));
+        list.add( new Course("Aws","Devops","2","114"));
+        list.add( new Course("Selenium","Automation","4","125"));
+        list.add( new Course("Cypress","Automation","4","22"));
         list.add( new Course("Cucumber","Automation","3","36"));
-        list.add( new Course("Java","Development","4.1","268"));
-        list.add( new Course("C#","Development","3.9","99"));
+        list.add( new Course("Java","Development","4","268"));
+        list.add( new Course("C#","Development","3","99"));
+
+        // allMatch, nonMatch, anyMatch
+
+        // Problem 1-> I want the courses whose score is greater than 3
+        list.stream().filter(course->Integer.parseInt(course.getReviewScore())>3).
+                map(course -> course.getName()+"--"+course.getReviewScore()).
+                forEach(System.out::println);
+
+        // This will return a boolean value based on matches. [allMatch]
+        System.out.println(
+                list.stream().allMatch(course -> Integer.parseInt(course.getReviewScore())>2)
+        );
+        // This will return a boolean value based on matches. [nonMatch]
+        System.out.println(
+                list.stream().noneMatch(course -> Integer.parseInt(course.getReviewScore())<1)
+        );
+        // This will return a boolean value based on matches. [anyMatch]
+        System.out.println(
+                list.stream().anyMatch(course -> Integer.parseInt(course.getReviewScore())>=4)
+        );
     }
 }
