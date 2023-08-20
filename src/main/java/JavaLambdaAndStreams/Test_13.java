b1d3ca88807ffee9da86982ba30178feb083ac10
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-20 16:03:42 +0530

    added few more topics

--- src/main/java/JavaLambdaAndStreams/Test_13.java
+++ src/main/java/JavaLambdaAndStreams/Test_13.java
@@ -1,4 +1,19 @@
 package JavaLambdaAndStreams;
 
+import JavaLambdaAndStreams.course.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test_13 {
+    public static void main(String args[]){
+        List<Course> list = new ArrayList<>();
+        list.add( new Course("Azure","Devops","3","101"));
+        list.add( new Course("Aws","Devops","2.5","114"));
+        list.add( new Course("Selenium","Automation","3.9","125"));
+        list.add( new Course("Cypress","Automation","3.5","22"));
+        list.add( new Course("Cucumber","Automation","3","36"));
+        list.add( new Course("Java","Development","4.1","268"));
+        list.add( new Course("C#","Development","3.9","99"));
+    }
 }
---
+++ src/main/java/JavaLambdaAndStreams/course/Course.java
@@ -0,0 +1,56 @@
+package JavaLambdaAndStreams.course;
+
+public class Course {
+    private String name;
+    private String category;
+    private String reviewScore;
+    private String noOfStudents;
+
+    public Course(String name, String category, String reviewScore, String noOfStudents) {
+        this.name = name;
+        this.category = category;
+        this.reviewScore = reviewScore;
+        this.noOfStudents = noOfStudents;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", reviewScore='" + reviewScore + '\'' +
+                ", noOfStudents='" + noOfStudents + '\'' +
+                '}';
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getReviewScore() {
+        return reviewScore;
+    }
+
+    public void setReviewScore(String reviewScore) {
+        this.reviewScore = reviewScore;
+    }
+
+    public String getNoOfStudents() {
+        return noOfStudents;
+    }
+
+    public void setNoOfStudents(String noOfStudents) {
+        this.noOfStudents = noOfStudents;
+    }
+}
