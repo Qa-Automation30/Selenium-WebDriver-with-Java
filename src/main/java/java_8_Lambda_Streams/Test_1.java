8fa5ec4bc9a0a4f983e0fa67d9b5cf55f25713d2
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-23 19:17:55 +0530

    added few more topics

---
+++ src/main/java/java_8_Lambda_Streams/Test_1.java
@@ -0,0 +1,14 @@
+package java_8_Lambda_Streams;
+
+public class Test_1 {
+    /**
+     * What is lambda ->
+     * a) Lambda is equivalent to a function(method) without a name.
+     * b) Lambdas are also referred as Anonymous functions.  Lambda has all Qualities like regular method, such As :
+     * 1) Method parameters
+     * 2) Method Body
+     * 3) return Type
+     * c) Lambdas are not tied to any class like a regular method.
+     * d) Lambda can also be assigned to variable and passed around in any other class.
+     */
+}
