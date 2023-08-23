3963c8f2c7b9644b523cfbd28abbd963f64d5589
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-23 23:01:29 +0530

    added few more topics

---
+++ src/main/java/java_8_Lambda_Streams/Test_6_BiConsumer.java
@@ -0,0 +1,21 @@
+package java_8_Lambda_Streams;
+
+public class Test_6_BiConsumer {
+    /**
+     *  BiConsumer interface is the extension of the Consumer interface.
+     *
+     *  The BiConsumer interface is an extension of the Consumer interface in Java,
+     *  and it's used when you need to perform an operation that takes two input arguments and doesn't return any result.
+     *
+     *  The "Bi" in BiConsumer stands for "binary," indicating that it deals with two inputs.
+     *
+     *  The primary difference between Consumer and BiConsumer is the number of input arguments they accept:
+     *
+     * Consumer<T>: Accepts a single input of type T.
+     * BiConsumer<T, U>: Accepts two inputs of types T and U.
+     *
+     * The BiConsumer interface is part of the java.util.function package and is often used in scenarios where
+     * you need to perform some operation that involves two input parameters,
+     * such as updating two values in a map, performing actions on pairs of elements, or applying operations on pairs of arguments.
+     */
+}
