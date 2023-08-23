71242edf0f14684567d26dd7cc1a7ccea1a04f30
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-23 19:55:35 +0530

    added few more topics

--- src/main/java/java_8_Lambda_Streams/Test_2.java
+++ src/main/java/java_8_Lambda_Streams/Test_2.java
@@ -2,6 +2,35 @@ package java_8_Lambda_Streams;
 
 import org.testng.annotations.Test;
 
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Test_2 {
+
+       public static void main(String [] args){
+           /**
+            * 1st way to write the sum method
+            */
+           int addition = sum(2,3);
+           System.out.println(addition);
+
+           /**
+            * 2nd way to write the sum method with help of lambda
+            */
+           BinaryOperator<Integer> sum = (a,b)-> a + b;
+           sum.apply(10,10);
+           /**
+            * 3rd way to write the sum method with help of lambda
+            */
+           BinaryOperator<Integer> sum1 = Integer::sum;
+           sum1.apply(10,10);
+       }
+       public static int sum(int a, int b){
+           return a+b;
+       }
+
+
 
 }
