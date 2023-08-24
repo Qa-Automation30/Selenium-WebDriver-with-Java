6f1cfda28ac6e929aad05d9d2fbff65dd85ece76
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-24 23:20:11 +0530

    added few more topics

---
+++ src/main/java/java_8_Lambda_Streams/someImp/AssertionsWithLambdaAndStreams.java
@@ -0,0 +1,44 @@
+package java_8_Lambda_Streams.someImp;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class AssertionsWithLambdaAndStreams {
+    /**
+     * How we can achieve multiple assertions for different elements via one function using streams and Lambada ?
+     */
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.udemy.com/");
+        driver.manage().window().maximize();
+
+        List<WebElement> elementsToAssert = driver.findElements(By.xpath("//h3[text()='Development']//parent::div[@class='trending-topics--category--3sEMw']//a"));
+
+        // Define expected values for assertions
+        List<String> expectedValues = Arrays.asList("Python", "Excel", "Web Development");
+        Uninterruptibles.sleepUninterruptibly(Duration.ofSeconds(5));
+
+      List<String> allValues = elementsToAssert.
+              stream().limit(4).
+              map(Functions.getAttributeValues).
+              collect(Collectors.toList());
+      System.out.println(allValues);
+
+        Assert.assertEquals(expectedValues,elementsToAssert.stream().map(Functions.getAttributeValues).collect(Collectors.toList()));
+
+
+        driver.quit();
+    }
+
+}
---
+++ src/main/java/java_8_Lambda_Streams/someImp/Functions.java
@@ -0,0 +1,31 @@
+package java_8_Lambda_Streams.someImp;
+
+import org.openqa.selenium.WebElement;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Functions {
+    /**
+     * 1st way to write this
+     */
+   public static Function<WebElement,String> getAttributeValues = WebElement::getText;
+    /**
+     * 2nd way to write this as taking parameters
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     * public static Function<WebElement, String> getAttributeValue(String attributeValue): This line declares a static method named getAttributeValue.
+     * The method takes a single parameter of type String named attributeValue. It returns a Function that takes a WebElement as input and returns a String.
+     *
+     * return e -> e.getAttribute(attributeValue);: This is the body of the getAttributeValue method.
+     * It's a lambda expression that creates and returns a Function instance.
+     *
+     * e -> e.getAttribute(attributeValue) is the lambda expression itself.
+     * The e is the parameter name for the WebElement passed to the function.
+     * e.getAttribute(attributeValue) is the implementation of the function.
+     * It calls the getAttribute method on the provided WebElement and passes the attributeValue parameter to it.
+     * The attributeValue parameter determines which attribute's value will be retrieved from the WebElement.
+     */
+    public static Function<WebElement,String> getAttributeValue(String attributeValue) {
+       return e-> e.getAttribute(attributeValue);
+    }
+}
