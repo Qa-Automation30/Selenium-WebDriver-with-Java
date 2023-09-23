e683c025c26841ff403d7e5677696662570d39b2
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-09-23 09:52:21 +0530

    added few more topics

---
+++ src/main/java/java_8_Lambda_Streams_Pragmatic_Code_School/section_11/Video_32.java
@@ -0,0 +1,8 @@
+package java_8_Lambda_Streams_Pragmatic_Code_School.section_11;
+
+public class Video_32 {
+    /**
+     * 1-> Streams API introduced in Java 8
+     * 2-> Main purpose is to perform some operation on Collections
+     */
+}
